8ac380d6f13187811e236aaee9fdcf13a31a0727
Nguyen Truong Son <gager981@Nguyens-MacBook-Pro.local>
2023-04-26 20:45:25 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:f2_flutter_ecommerce_app/packages/quote/quote.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'pages/landing_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Quotes().getAll();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LandingPage(),
+    );
+  }
+}
